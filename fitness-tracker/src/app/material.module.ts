d0c0ca30bbc970677aeffc5ef5416e9d18656473
Abhishek Chakraborty <chakraborty@silexica.com>
2019-03-13 16:28:17 +0100

    Initial Angular7 Mean App

---
+++ fitness-tracker/src/app/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import {MatIconModule, MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatDatepickerModule, MatSidenavModule, MatToolbarModule, MatListModule } from '@angular/material';
+@NgModule({
+    imports: [MatIconModule,MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatDatepickerModule, MatToolbarModule, MatSidenavModule,MatListModule],
+    exports: [MatIconModule,MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatDatepickerModule, MatToolbarModule, MatSidenavModule,MatListModule],
+
+})
+
+export class MaterialModule { }
